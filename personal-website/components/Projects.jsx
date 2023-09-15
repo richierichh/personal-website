84c2f7ef361ec217bcf39f70b7007c07afd9e060
richierichh <78582740+richierichh@users.noreply.github.com>
2023-09-15 16:23:46 -0400

    finished v1 of website

--- personal-website/components/Projects.jsx
+++ personal-website/components/Projects.jsx
@@ -6,9 +6,9 @@ import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
 
 const projectItems = [
     {
-      name: "MERN Full Stack To Do List",
+      name: "MERN Full Stack Bug Ticket List",
       description:
-        "A full stack to do list tracker that allows users to create and delete tasks built with React, Node.js, Express, and MongoDB.",
+        "A full stack bug ticket tracker that allows users to create and delete tasks built with React, Node.js, Express, and MongoDB.",
       image: "/todolist.png",
       github: "https://github.com/richierichh/MernToDo",
       link: "https://richardsmerntodolist.netlify.app/#",
@@ -18,8 +18,8 @@ const projectItems = [
     description:
       "An application that summarizes and converts PDF research papers to powerpoint presentations",
     image: "/PDF2PPTX.png",
-    github: "https://github.com/richierichh/MernToDo",
-    link: "https://richardsmerntodolist.netlify.app/#",
+    github: "https://github.com/richierichh/PDF2PPT",
+    link: "https://github.com/richierichh/PDF2PPT",
   },
     {
       name: "Stock Price Predictor",
@@ -54,6 +54,7 @@ const projectItems = [
                   <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
                     <div className=" md:w-1/2">
                       <Link href={project.link}>
+                    
                         <Image
                           src={project.image}
                           alt=""
@@ -61,13 +62,29 @@ const projectItems = [
                           height={1000}
                           className="rounded-xl shadow-xl hover:opacity-70"
                         />
+                        
                       </Link>
                     </div>
                     <div className="mt-8 md:w-1/2">
                       <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
                       <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
                         {project.description}
+                        
                       </p>
+                      <div className="flex flex-row align-bottom space-x-4">
+                      <Link href={project.github} target="_blank">
+                        <BsGithub
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                      <Link href={project.link} target="_blank">
+                        <BsArrowUpRightSquare
+                          size={30}
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      </Link>
+                    </div>
                      
                     </div>
                   </div>
